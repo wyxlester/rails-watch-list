b19494849bc68d8a960ae599aa3402c5a1f900f3
Lester Wee <lester.wee.68@gmail.com>
2022-11-17 23:41:06 +0800

    completed user authentication.

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,4 +1,5 @@
 class ListsController < ApplicationController
+  skip_before_action :authenticate_user!, only: :index
   def index
     @lists = List.all
   end
---
+++ db/migrate/20221117151718_add_column_for_user.rb
@@ -0,0 +1,4 @@
+class AddColumnForUser < ActiveRecord::Migration[7.0]
+  def change
+  end
+end
